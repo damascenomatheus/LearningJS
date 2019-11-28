1ea5da6c4dc47c2589f1cd276cfd596005f5d20f
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-11-27 21:58:38 -0300

    Adding exercise

--- ThreePillarsJS.js
+++ ThreePillarsJS.js
@@ -1,13 +1,43 @@
-function ask(question) {
-    console.log(this.teacher,this.son, question)
+class Bookshelf {
+	constructor() {
+		this.favoriteBooks = [];
+	}
+
+	// TODO: define methods `addFavoriteBook(..)`
+	// and `printFavoriteBooks()`
 }
 
-function otherClass() {
-    var myContext = {
-        teacher: "Suzy",
-        son: "Marie",
-    }
-    ask.call(myContext,"Why?")
+function addFavoriteBook(bookName) {
+	if (!bookName.includes("Great")) {
+		favoriteBooks.push(bookName);
+	}
 }
 
-otherClass()
+function printFavoriteBooks() {
+	console.log(`Favorite Books: ${favoriteBooks.length}`);
+	for (let bookName of favoriteBooks) {
+		console.log(bookName);
+	}
+}
+
+function loadBooks( /* .. */ ) {
+	// TODO: call fakeAjax( .. );
+}
+
+var BOOK_API = "https://some.url/api";
+
+
+// ***********************
+
+// NOTE: don't modify this function at all
+function fakeAjax(url,cb) {
+	setTimeout(function fakeLoadingDelay(){
+		cb([
+			"A Song of Ice and Fire",
+			"The Great Gatsby",
+			"Crime & Punishment",
+			"Great Expectations",
+			"You Don't Know JS"
+		]);
+	},500);
+}
